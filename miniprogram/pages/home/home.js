c8774dddaccdbad2629ccf07da68d6f847b8e674
unknown <unknown@example.com>
2021-08-12 10:38:47 +0800

    Initial Commit

---
+++ miniprogram/pages/home/home.js
@@ -0,0 +1,89 @@
+// pages/home/home.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    msg:"hello world",
+    num:100,
+    isGirl:false,
+    person:{
+      age:80,
+      height:150,
+      weight:200,
+      neme:"富婆"
+    },
+    ischecked:false,
+    list:[
+      {
+        id:0,
+        name:"猪八戒"
+      },
+      {
+        id:1,
+        name:"天蓬元帅"
+      },
+      {
+        id:2,
+        name:"悟能"
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
